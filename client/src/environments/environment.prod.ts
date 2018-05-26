bda6425db5c9bfc9e63ff82c968e6f4632b8d423
lucas messias <lucasmessias.pg@outlook.com>
2018-05-26 14:27:58 -0300

    fix socket

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   api: "http://scoreboardtcg.kinghost.net/api",
-  domain: "http://scoreboardtcg.kinghost.net/api",
+  domain: "http://scoreboardtcg.kinghost.net/",
   web_link: "http://scoreboardtcg.kinghost.net"
 };
