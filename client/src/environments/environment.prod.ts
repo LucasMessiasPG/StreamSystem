4e8c908468e700278740e2db6519dcdd9b4c32d3
lucas messias <lucasmessias.pg@outlook.com>
2018-05-26 13:48:10 -0300

    ...

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   api: "https://scoreboardtcg.kinghost.net/api",
-  domain: "https://scoreboardtcg.kinghost.net",
+  domain: "https://scoreboardtcg.kinghost.net/api",
   web_link: "https://scoreboardtcg.kinghost.net"
 };
